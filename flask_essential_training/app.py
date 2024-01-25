80ee6a91f84bc63e3a88c69c6cf6f971746cbe15
AlvinKimata <alvinkimata@gmail.com>
2024-01-25 12:59:04 +0300

    using GET and POST requests in Flask

--- flask_essential_training/app.py
+++ flask_essential_training/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', name="Kimata")
 
-@app.route('/your_url', methods=['GET'])
+@app.route('/your_url', methods=['GET', 'POST'])
 def your_url():
-    url = request.args.get('url')
-    code = request.args.get('code')
-    return render_template('your_url.html', url=url, code=code)
+    # url = request.args.get('url')
+    code = request.form['code']
+    return render_template('your_url.html', code=code)
 
 if __name__ == '__main__':
     app.run(debug=True)
