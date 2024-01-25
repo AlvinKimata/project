96c074d3b9643ceb660f6d0417d19faa3560c538
AlvinKimata <alvinkimata@gmail.com>
2024-01-25 12:45:48 +0300

    passing form variables

--- flask_essential_training/app.py
+++ flask_essential_training/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name = "Kimata")
+    return render_template('home.html', name="Kimata")
 
-@app.route('/about')
-def about():
-    return 'This is a url shortener'
+@app.route('/your_url', methods=['GET'])
+def your_url():
+    url = request.args.get('url')
+    code = request.args.get('code')
+    return render_template('your_url.html', url=url, code=code)
+
+if __name__ == '__main__':
+    app.run(debug=True)
