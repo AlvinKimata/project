9b1fc4927ad27809bf51adc7d4f9bcbd5a429b34
AlvinKimata <alvinkimata@gmail.com>
2024-01-30 08:41:23 +0300

    add pages blueprint

--- rp_flask_board/board/__init__.py
+++ rp_flask_board/board/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello world!'
-
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8000, debug = True)
+def create_app():
+    app = Flask(__name__)
+    return app
