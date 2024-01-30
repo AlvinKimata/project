86108e637817558057c29f1fdaea5b03a7e2b365
AlvinKimata <alvinkimata@gmail.com>
2024-01-30 11:07:31 +0300

    code to insert data from views to db

--- rp_flask_board/board/database.py
+++ rp_flask_board/board/database.py
@@ -3,6 +3,7 @@ import sqlite3
 from flask import current_app, g
 
 def init_app(app):
+    app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
 
@@ -23,5 +24,12 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
-        
+
     return g.db
+
+
+def close_db(e = None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
