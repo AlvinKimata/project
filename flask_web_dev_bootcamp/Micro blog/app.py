c13be811f2894f97dd3b85ef825edacf403a5e94
AlvinKimata <alvinkimata@gmail.com>
2024-02-01 10:50:54 +0300

    using expressions in jinja2

--- flask_web_dev_bootcamp/Micro blog/app.py
+++ flask_web_dev_bootcamp/Micro blog/app.py
@@ -20,3 +20,32 @@ def render_jinja2_intro():
     return render_template(
         "jinja2-intro.html", name = "John Doe",
         template_name = "Jinja2")
+
+@app.route("/expressions/")
+def render_expressions():
+    # interpolation
+    color = "brown"
+    animal_one = "fox"
+    animal_two = "dog"
+
+    # addition and subtraction
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 15
+
+    # string concatenation
+    first_name = "Captain"
+    last_name = "Marvel"
+
+    kwargs = {
+        "color": color,
+        "animal_one": animal_one,
+        "animal_two": animal_two,
+        "orange_amount": orange_amount,
+        "apple_amount": apple_amount,
+        "donate_amount": donate_amount,
+        "first_name": first_name,
+        "last_name": last_name
+    }
+
+    return render_template("expressions.html", **kwargs)
