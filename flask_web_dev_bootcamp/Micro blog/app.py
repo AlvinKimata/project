986e7bc5f8de600a059c1bbaff19b0fbc3ccb6e0
AlvinKimata <alvinkimata@gmail.com>
2024-02-01 10:39:31 +0300

    add values dynamically to a html string

--- flask_web_dev_bootcamp/Micro blog/app.py
+++ flask_web_dev_bootcamp/Micro blog/app.py
@@ -12,5 +12,11 @@ def render_first_page():
     return render_template("first_page.html")
 
 @app.route("/second_page")
-def render_first_page():
+def render_second_page():
     return render_template("second_page.html")
+
+@app.route("/jinja2")
+def render_jinja2_intro():
+    return render_template(
+        "jinja2-intro.html", name = "John Doe",
+        template_name = "Jinja2")
