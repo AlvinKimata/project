092481dcf86b11298539417d68ef8320c81885c3
AlvinKimata <alvinkimata@gmail.com>
2024-02-07 11:44:37 +0300

    for-loop using Jinja

--- flask_digitalocean_series/2. Using templates in a Flask app/app.py
+++ flask_digitalocean_series/2. Using templates in a Flask app/app.py
@@ -10,3 +10,13 @@ def hello():
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+
+@app.route('/comments/')
+def comments():
+    comments = ['This is the first comment.',
+                'This is the second comment.',
+                'This is the third comment.',
+                'This is the fourth comment.'
+                ]
+    return render_template('comments.html', comments = comments)
