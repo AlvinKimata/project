1cefcaf9bbc7f1d93897b2c4dd51603f8727745e
AlvinKimata <alvinkimata@gmail.com>
2024-02-07 13:05:14 +0300

    using web forms in a Flask app

--- flask_digitalocean_series/4. Using web forms/app.py
+++ flask_digitalocean_series/4. Using web forms/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
 
 
 messages = [{'title': 'Message One',
@@ -15,4 +16,16 @@ def index():
 
 @app.route('/create/', methods = ('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required.')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+        
     return render_template('create.html')
