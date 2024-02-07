1085f42b24f882349f0a83bd1270297150945540
AlvinKimata <alvinkimata@gmail.com>
2024-02-07 11:32:50 +0300

    linking between pages

--- flask_digitalocean_series/2. Using templates in a Flask app/app.py
+++ flask_digitalocean_series/2. Using templates in a Flask app/app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html', utc_dt = datetime.utcnow())
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
