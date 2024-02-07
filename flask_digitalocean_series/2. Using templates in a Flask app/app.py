363739575a19c2d46fc8af2b99bffc8d806d5dd8
AlvinKimata <alvinkimata@gmail.com>
2024-02-07 11:20:10 +0300

    render a template and using variables

---
+++ flask_digitalocean_series/2. Using templates in a Flask app/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html', utc_dt = datetime.utcnow())
